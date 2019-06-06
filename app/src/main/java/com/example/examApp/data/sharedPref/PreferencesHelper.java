83227de956b5b061abe26a08b4c545cad5cd0627
Chen Ben-Ami <chen24201@gmail.com>
2019-06-06 13:57:49 +0300

    shared prefs interface

---
+++ app/src/main/java/com/example/examApp/data/sharedPref/PreferencesHelper.java
@@ -0,0 +1,12 @@
+package com.example.examApp.data.sharedPref;
+
+public interface PreferencesHelper {
+
+    String getAppTitle();
+
+    void setAppTitle(String appTitle);
+
+    String getAppImage();
+
+    void setAppImage(String appImage);
+}
